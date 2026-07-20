c99cdba02bbca585c30b628458169377e1e5760a
yourName <yourEmail>
2026-07-20 16:32:07 +0300

    asdasvcvi

--- server_scripts/Крафт корпусов оружий.js
+++ server_scripts/Крафт корпусов оружий.js
@@ -18,8 +18,6 @@ ServerEvents.recipes(e => {
 })
 
 
-
-
 ServerEvents.recipes(e => {
   e.recipes.createCutting('catr:corpus_akm', 'create:iron_sheet')
 })
@@ -37,4 +35,10 @@ ServerEvents.recipes(e => {
 })
 ServerEvents.recipes(e => {
   e.recipes.createCutting('catr:corpus_cz_75', 'create:iron_sheet')
+})
+ServerEvents.recipes(e => {
+  e.recipes.createCutting('catr:wooden_handle', 'minecraft:oak_wood')
+})
+ServerEvents.recipes(e => {
+  e.recipes.createCutting('catr:wooden_handle', 'minecraft:oak_wood')
 })
